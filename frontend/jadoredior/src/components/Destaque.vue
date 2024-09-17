5d8b35d89ae65407b35fa29308c213c348c4b960
Heric da Silva Cruz <hericsilvaho@gmail.com>
2024-09-17 15:51:59 -0300

    corrigindo erro de português

--- frontend/jadoredior/src/components/Destaque.vue
+++ frontend/jadoredior/src/components/Destaque.vue
@@ -1,39 +1,43 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from "vue-router";
 </script>
 
 <template>
-    <div class="destaque">
-        <RouterLink to="/">
-        <img src="https://lojadior.vteximg.com.br/arquivos/jdr-or-s-2-24-model-pack-5550-x-2000-1.jpg" alt="">
-        </RouterLink>
-        <div class="info">
-            <p>Fragancia J'adore L'or</p>
-        </div>   
+  <div class="destaque">
+    <RouterLink to="/">
+      <img
+        src="https://lojadior.vteximg.com.br/arquivos/jdr-or-s-2-24-model-pack-5550-x-2000-1.jpg"
+        alt=""
+      />
+    </RouterLink>
+    <div class="info">
+      <p>Fragrância J'adore L'or</p>
     </div>
+  </div>
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Playwrite+CU:wght@100..400&display=swap");
 .destaque {
-    margin-top: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 .destaque img {
-    max-width: 100%;
-    height: 100%;
-    border-radius: 10px;
-    box-shadow: 0 0 10px black;
+  max-width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  box-shadow: 0 0 10px black;
 }
 .destaque .info p {
-    font-family: "Playwrite CU", cursive;
-    font-optical-sizing: auto;
-    font-weight: 200;
-    font-style: normal;
-    text-decoration: none;
-    color: black;
+  font-family: "Playwrite CU", cursive;
+  font-optical-sizing: auto;
+  font-weight: 200;
+  font-style: normal;
+  text-decoration: none;
+  color: black;
 }
 </style>
+
